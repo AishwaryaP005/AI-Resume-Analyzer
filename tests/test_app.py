845f1a8498f416982b411610a24a2f02ac3d2aaf
DevByPurva <167107059+DevByPurva@users.noreply.github.com>
2026-04-13 14:11:46 +0530

    Comment out test_home_route function
    Comment out the test_home_route function in test_app.py.

--- tests/test_app.py
+++ tests/test_app.py
@@ -67,9 +67,10 @@ def client():
         yield client
 
 
-def test_home_route(client):
-    response = client.get("/")
-    assert response.status_code == 200
+# def test_home_route(client):
+# response = client.get("/")
+# assert response.status_code == 200
+
 
 
 def test_upload_no_file(client):
