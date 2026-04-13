da9cc2363ff712d80739861562f869408c662332
DevByPurva <167107059+DevByPurva@users.noreply.github.com>
2026-04-13 15:18:38 +0530

    Use environment variable for app port configuration

--- backend/app.py
+++ backend/app.py
@@ -96,7 +96,8 @@ def upload():
 
 # ----------- RUN APP -----------
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 # from flask import Flask, request, jsonify, send_from_directory
 # from flask_cors import CORS
 # import os
